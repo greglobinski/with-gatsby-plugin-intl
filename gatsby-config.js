75ddf9f67127048e93e0f5c209d77d85315e3010
greg lobinski <greglobinski@gmail.com>
2020-06-05 11:25:48 +0200

    test: redirect commented

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
         // language file path
         defaultLanguage: `en`,
         // option to redirect to `/ko` when connecting `/`
-        redirect: false,
+        // redirect: false,
       },
     },
   ],
